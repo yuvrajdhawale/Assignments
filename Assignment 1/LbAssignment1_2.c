d82a785a02d2524eac01fd3162bb8a0734ae2e60
yuvrajdhawale <yuvraj.d1323@gmail.com>
2026-02-03 22:28:16 +0530

    Add files via upload

---
+++ Assignment 1/LbAssignment1_2.c
@@ -0,0 +1,22 @@
+//program to print 5 times marvellous on screen
+
+#include<stdio.h>
+
+void Display()
+{
+    int i=0;
+    for(i=1;i<=5;i++)
+    {
+        printf("Marvellous\n");
+    }
+}
+
+
+
+int main()
+{
+    Display();
+
+
+    return 0;
+}
---
+++ Assignment 1/LbAssignment1_3.c
@@ -0,0 +1,20 @@
+//program to print number from 5 to 1 on screen
+
+#include<stdio.h>
+
+void Display()
+{
+    int i = 0;
+    i = 5;
+    while(i >= 1 )
+    {
+        printf("%d\n",i);
+        i-- ;
+    }
+}
+int main()
+{
+    Display ();
+
+return 0;
+}
